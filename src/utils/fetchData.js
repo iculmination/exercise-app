1d8c4ffa0d8367ee4de2308e0bbced4767be66a8
iculmination <ovsiichuk.bohdan@gmail.com>
2024-08-03 01:16:31 +0300

    fetchdata

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,11 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+    "x-rapidapi-key": process.env.EXERCISEDB_KEY,
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
